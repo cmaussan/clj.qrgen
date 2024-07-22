df7ea79fed1069b8a3116c04bf1c6d3c7f63bdd2
cmaussan <cmaussan@gmail.com>
2024-07-22 14:28:18 +0200

    updated: QRGen repos and version

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/killme2008/clj.qrgen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"jitpack.io" "https://jitpack.io"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.glxn.qrgen/javase "2.0"]])
+                 [com.github.kenglxn.QRGen/javase "3.0.1"]])
